5393b8a770857813a94637bbf19c7130e348f331
manju6 <manjube.kce@gmail.com>
2018-03-29 22:59:32 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -10,12 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/webhook")
 public class HelloWorldController {
 
-    @RequestMapping(value="/getTotalOrders",method=RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody WebhookResponse getTotalOrders(@RequestBody String obj){
-
+    
+        @RequestMapping(method = RequestMethod.POST)
+        public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
         System.out.println(obj);
-
-        return new WebhookResponse("Total Orders in Past 24 Hours is 4500 " , "Total Orders in Past 24 Hours is 4500");
+        return new WebhookResponse("Total Orders in Past 24 Hours is 4500" , "Total Orders in Past 24 Hours is 4500");
     }
 
 }
