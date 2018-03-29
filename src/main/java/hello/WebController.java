8b02c75737c3ba1a74fcfb8337223ff16c9e1eb7
manju6 <manjube.kce@gmail.com>
2018-03-29 22:33:31 +0530

    Created WebController

---
+++ src/main/java/hello/WebController.java
@@ -0,0 +1,26 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.coe.cpm.dao.RepositoryImpl;
+import com.tcs.coe.cpm.utility.DateParsing;
+
+public class WebController 
+{
+	public ResponseEntity<?> getTotalOrders() 
+	{
+				return new ResponseEntity<>(repository.getTotalOrders(), HttpStatus.OK);
+	}
+
+}
