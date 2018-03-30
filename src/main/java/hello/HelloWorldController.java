5cb29231f6a486c4bb2857faf0046e87a4d41605
manju6 <manjube.kce@gmail.com>
2018-03-30 12:42:29 +0530

    Update HelloWorldController.java

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,6 +1,9 @@
 package hello;
 
-import org.apache.tomcat.util.http.parser.MediaType;
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,18 +23,27 @@ public class HelloWorldController {
         public @ResponseBody WebhookResponse groupByOrderChannel(@RequestBody String obj){
         System.out.println(obj);
         RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("https://cpm-json-data.herokuapp.com/api/groupByOrderChannel", Object[].class);
-		Object[] objects = responseEntity.getBody();
-		return new WebhookResponse("Group By Orders Channel and Its Count" +objects, "Group By Orders Channel and Its Count" +objects);
+		ResponseEntity<List<WebhookResponse>> rateResponse =
+		        restTemplate.exchange("https://cpm-json-data.herokuapp.com/api/groupByOrderChannel",
+		                    HttpMethod.GET, null, new ParameterizedTypeReference<List<WebhookResponse>>() {
+		            });
+		List<WebhookResponse> rates = rateResponse.getBody();
+		
+		return new WebhookResponse("Group By Orders Channel and Its Count" +rates, "Group By Orders Channel and Its Count" +rates);
     }
     
         @RequestMapping(value="/getTotalOrders",method=RequestMethod.POST)
         public @ResponseBody WebhookResponse getTotalOrders(@RequestBody String obj){
         System.out.println(obj);
-        RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("https://cpm-json-data.herokuapp.com/api/getTotalOrders", Object[].class);
-		Object[] objects = responseEntity.getBody();
-		return new WebhookResponse("Group By Orders Channel and Its Count" +objects, "Group By Orders Channel and Its Count" +objects);
+      
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<List<WebhookResponse>> rateResponse =
+		        restTemplate.exchange("https://cpm-json-data.herokuapp.com/api/groupByOrderChannel",
+		                    HttpMethod.GET, null, new ParameterizedTypeReference<List<WebhookResponse>>() {
+		            });
+		List<WebhookResponse> rates = rateResponse.getBody();
+		
+		return new WebhookResponse("Group By Orders and Its Count" +rates, "Group By Orders Channel and Its Count" +rates);
     }
 
 }
